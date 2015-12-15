50193d59b379b41aa562d0167a9a3170c4dceab4
Oduola Akeem <akeem.oduola@andela.com>
2015-12-15 19:20:18 +0100

    Redirect Service shortened URL
    WHY: So when users follow a service shortened URL, they are redirected to the original URL
    
    This change addresses the need by:
    * Adding a method redirect_link to the LinksController class
    * Adding a method display_link to the LinksHelper module
    * Modifying the links show view to display the link
    
    [Finishes #110112092]

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "pages#index"
 
   resources :links
+  get "/:path", to: "links#redirect_link"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

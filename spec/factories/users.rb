5296e8f60a38106826318c67074601a9b5b50b72
Oduola Akeem <akeem.oduola@andela.com>
2015-12-17 13:15:41 +0100

    Test Controllers

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    provider "MyString"
-uid "MyString"
-name "MyString"
-image_url "MyString"
+    provider "facebook"
+    uid "1234567"
+    name "user mock"
+    image_url "http://graph.facebook.com/1234567/picture?type=square"
   end
-
 end

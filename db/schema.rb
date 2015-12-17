5fba85e330b61244aadcccdaae3896bfd5320648
Oduola Akeem <akeem.oduola@andela.com>
2015-12-17 03:43:31 +0100

    Service shortened URL details
    Why: So registered users can view usage details about each URL
    
    This change addresses the need by:
    *Generating a migration to add ip address and country of users to the click table
    *Modifying the LinksCotroller#redirect to capture the details of each click before redirect
    *Defining methods in ClicksHelper to get the ip and country where click originated from.
    *Defining methods in Click model class to set the ip and country of click and also setup charts for display in details view
    *Modifying the Links show view to display the usage information.
    
    [Finishes #110112658]
    [Finishes #110112570]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216163002) do
+ActiveRecord::Schema.define(version: 20151217021309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20151216163002) do
     t.integer  "link_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.inet     "ip"
+    t.string   "country"
   end
 
   add_index "clicks", ["link_id"], name: "index_clicks_on_link_id", using: :btree

68571b9925846f3767ffc61686bc207b02cf266f
Oduola Akeem <akeem.oduola@andela.com>
2015-12-17 13:15:41 +0100

    Test Controllers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,13 @@ require 'spork'
 require 'database_cleaner'
 
 Spork.prefork do
+  require "coveralls"
+  require "phantomjs"
+  Coveralls.wear!
+
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
@@ -18,10 +25,25 @@ Spork.prefork do
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
+
+    require "capybara/poltergeist"
+    Capybara.javascript_driver = :poltergeist
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app,
+                js_errors: true, port: 446_78 + ENV["RAILS_ENV"].to_i,
+                  phantomjs_options: ["--proxy-type=none"], timeout: 180)
+    end
   end
 end
 
 Spork.each_run do
+  require "coveralls"
+  require "phantomjs"
+  Coveralls.wear!
+
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+
   FactoryGirl.reload
   DatabaseCleaner.clean
 end

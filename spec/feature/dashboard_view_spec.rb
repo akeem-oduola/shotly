ab94a2bfabd502519175c2ea074bfe4022d59dfa
Oduola Akeem <akeem.oduola@andela.com>
2016-01-07 17:42:08 +0100

    Clean code smells

--- spec/feature/dashboard_view_spec.rb
+++ spec/feature/dashboard_view_spec.rb
@@ -3,27 +3,22 @@ require "rails_helper"
 RSpec.feature "DashboardView" do
   before do
     @user = FactoryGirl.create(:user)
+    set_valid_omniauth
   end
 
   describe "Create user's links", js: true do
     it "creates a link with user_id" do
-      set_valid_omniauth
-      fill_in("link_url", with: "#{FactoryGirl.build(:link)}")
+      visit "/"
+      click_on "Please Login with: Facebook"
+      fill_in("link[url]", with: "#{FactoryGirl.build(:link)}")
       click_on "Shorten Url"
       @link = @user.links.first
       expect(page).to have_content "Link was successfully created."
       expect(@link.user_id).to eq(@user.id)
+      click_on "Edit", @link
+      uncheck "link_active"
+      click_on "Update Link"
+      expect(page).to have_content "My Links"
     end
-  end
-
-  it "does not redirect an inactive link", js: true do
-    set_valid_omniauth
-    visit "/"
-    click_on "Please Login with: Facebook"
-    click_link "Edit", @link
-    uncheck "link_active"
-    click_on "Update Link"
-    get :redirect_link, path: @link.short_url
-    expect(response).not_to redirect_to @link.url
   end
 end
